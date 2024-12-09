2fb1a6cd07b253a5920d1ab3e8db51486c83c107
Meet-Kathiriya <zyxcab101819@gmail.com>
2024-12-09 19:53:50 +0530

    Project Circlet

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,12 @@
-
+for (int i=5; i>=1; i--) 
+    {
+        for (int j=1; j<=i; j++) 
+        {
+            if (j%2 != 0) {
+                cout << "1 ";
+            } else {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
